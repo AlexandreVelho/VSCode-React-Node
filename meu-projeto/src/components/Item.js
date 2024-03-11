2d4b5c00e22b3100652ca8223c308f379d6a55ae
Alexandre Donisete Velho <160554012+AlexandreVelho@users.noreply.github.com>
2024-03-11 02:45:18 -0300

    Definição de Tipos para Props em Componentes ReactJS
    Compreender como definir tipos para as props em componentes ReactJS.
    Aprender a realizar validação de props.
    Entender como definir valores padrão para props.

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,9 +1,23 @@
-function Item(props) {
-    return (
-        <>
-          <li>{props.marca}</li>
-        </>
-    )
+import PropTypes from 'prop-types';
+
+function Item({ marca, ano_lancamento }) {
+  return (
+    <>
+      <li>
+        {marca} - {ano_lancamento}
+      </li>
+    </>
+  );
 }
 
-export default Item
+Item.propTypes = {
+  marca: PropTypes.string.isRequired,
+  ano_lancamento: PropTypes.number,
+};
+
+Item.defaultProps = {
+  marca: 'Faltou a marca',
+  ano_lancamento: 0,  
+};
+
+export default Item;
